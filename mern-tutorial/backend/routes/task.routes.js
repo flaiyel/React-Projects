19de1a4407a58e9ddbd9ae8d512f160f45000397
flaiyel <abanyel49@gmail.com>
2019-12-29 02:18:19 -0600

    Routes

--- mern-tutorial/backend/routes/task.routes.js
+++ mern-tutorial/backend/routes/task.routes.js
@@ -4,14 +4,35 @@ const router = express.Router();
 //Models
 const Task = require('../models/task');
 
-router.get('/', (req, res) => {
-    Task.find(function(err, tasks) {
-        console.log(tasks, { name:'Hola'});
-    });
-    res.json({
-        status: 'API Works!'
-    });
-    //res.send('Hello world');
+router.get('/', async (req, res) => {
+    const tasks = await Task.find();
+    res.json(tasks);
 });
+
+router.get('/:id', async (req, res) => {
+    const task = await Task.findById(req.params.id);
+    res.json(task);
+});
+
+router.post('/', async (req,res) => {
+    const { title, description  } = req.body;
+    const task = new Task({ title, description });
+    await task.save();
+    res.json({ status: 'Saved' });
+});
+
+router.put('/:id', async (req, res) => {
+    await Task.findByIdAndUpdate(req.params.id, {$set: req.body });
+    const json = await Task.find();
+    console.log(json);
+    res.json('Updated');
+});
+
+router.delete('/:id', async (req, res) => {
+    await Task.findByIdAndDelete(req.params.id);
+    const json = await Task.find();
+    console.log(json);
+    res.json('Deleted');
+})
 
 module.exports = router;
